e1ec81df7aeb550bd231d45090e6a767738df88a
megainsworth <meg13@vt.edu>
2015-09-24 12:39:42 -0400

    changed initial view controller
    accidentally created an extra view controller and had to change back to
    original

---
+++ PamplinApp.iOS/pinkViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace PamplinApp.iOS
+{
+	partial class pinkViewController : UIViewController
+	{
+		public pinkViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
